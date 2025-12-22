6c456c48b60c8fd426c5a14687b9acb38eaf0cc9
Arthur Ferreira <arthur.ferreiramiran@gmail.com>
2025-12-22 18:28:58 +0200

    Update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/br-states-flags-demo/',
+  base: '/br-state-flags-demo/',
 })
